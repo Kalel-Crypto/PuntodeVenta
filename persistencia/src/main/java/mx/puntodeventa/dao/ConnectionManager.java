a8a0d9f0d348fdadcaff577e35413ea339c9b69a
Kalel Baltierra <master.creeperfavorite@gmail.com>
2026-04-11 23:04:40 -0700

    Se agrego la clase inventario, se hizo el diseño, se agregaron cosas en el InventarioBean y se conecta a la base de datos

--- persistencia/src/main/java/mx/puntodeventa/dao/ConnectionManager.java
+++ persistencia/src/main/java/mx/puntodeventa/dao/ConnectionManager.java
@@ -6,11 +6,27 @@ import java.sql.SQLException;
 
 public class ConnectionManager {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/";
-    private static final String USER = "brayan";
-    private static final String PASSWORD = "6864314146";
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/inv_db?useSSL=false&serverTimezone=UTC&autoReconnect=true";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Creeperisfavorite20040102030405";
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); //
+
+            Connection con = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Conexión exitosa a la BD");
+            return con;
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("No se encontró el driver de MySQL");
+            e.printStackTrace();
+            throw new SQLException(e);
+        } catch (SQLException e) {
+            System.out.println("Error al conectar a la BD");
+            e.printStackTrace();
+            throw e;
+        }
     }
 }
